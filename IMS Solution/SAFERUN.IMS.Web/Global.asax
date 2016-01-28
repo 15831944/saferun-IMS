7f8ae178e010cc93474124c4aa275e5580549770
neozhu <new163@163.com>
2016-01-28 11:57:47 +0800

    init

---
+++ IMS Solution/SAFERUN.IMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAFERUN.IMS.Web.MvcApplication" Language="C#" %>
---
+++ IMS Solution/SAFERUN.IMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAFERUN.IMS.Web.Startup))]
+namespace SAFERUN.IMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
